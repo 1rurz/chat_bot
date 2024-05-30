7287b6264c5c95f44267c85fb6128102c31442ff
1rurz <vlad.repev.1976@mail.ru>
2024-05-31 00:03:16 +0300

    мелкое исправ

--- test.py
+++ test.py
@@ -2,7 +2,5 @@ import requests
 
 
 def test_page_opening():
-    response = requests.get(
-        "http://localhost:8501"
-    )  # Замените на URL вашего приложения
+    response = requests.get("http://localhost:8501")
     assert response.status_code == 200, "Ошибка при открытии страницы"
