37a6b127ad4a8d0a48516b61e49ec6ea4482c7a1
1rurz <vlad.repev.1976@mail.ru>
2024-05-30 22:55:07 +0300

    tests

--- test.py
+++ test.py
@@ -1,26 +1,8 @@
-import pytest
-from selenium import webdriver
+import requests
 
 
-@pytest.fixture(scope="module")
-def streamlit_app():
-    options = webdriver.ChromeOptions()
-    options.add_argument("--headless")
-    driver = webdriver.Chrome(options=options)
-    driver.get("http://localhost:8501")
-    yield driver
-    driver.quit()
-
-
-def test_app_opening(streamlit_app):
-    assert streamlit_app.title == "Помошник"  # Проверка наличия заголовка приложения
-    assert (
-        streamlit_app.find_element_by_xpath(
-            "//input[@placeholder='Введите сообщение...']"
-        )
-        is not None
-    )  # Проверка наличия поля ввода сообщения
-    assert (
-        streamlit_app.find_element_by_xpath("//button[contains(text(), 'Отправить')]")
-        is not None
-    )  # Проверка наличия кнопки отправки сообщения
+def test_page_opening():
+    response = requests.get(
+        "http://localhost:8501"
+    )  # Замените на URL вашего приложения
+    assert response.status_code == 200, "Ошибка при открытии страницы"
